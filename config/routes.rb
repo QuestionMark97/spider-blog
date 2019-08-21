2afcc2a89c408bfdf82ee83dc3f4c303cf0d47d7
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2019-08-20 22:07:02 -0400

    Complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+
+	# Pages controller
 	root 'pages#home'
 	get 'about', to: 'pages#about'
 
+	# Articles controller
 	resources :articles
 
+	# Users controller
 	get 'signup', to: 'users#new'
 	resources :users, except: [:new]
+
+	# Sessions controller
+	get 'login', to: 'sessions#new'
+	post 'login', to: 'sessions#create'
+	delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
